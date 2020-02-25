8ca2329b39d667de5a8b0cda4c057a22a4aaab26
dutchdinosaur <carnplaynl@gmail.com>
2020-02-25 16:24:21 +0100

    vooral scaling

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -6,53 +6,61 @@ using System.Linq;
 
 public class Grabber : MonoBehaviour {
 
+    [SerializeField] private Grabber otherHand;
     [SerializeField] private bool grabbed = false;
     public GameObject grabbedObject;
+    private GameObject scalingObject;
+    private float scaleMultip;
+    private float startDistance;
 
-    [SerializeField] private float scaleSpeed = 0.1f;
     [SerializeField] private float scaleMin = 0.01f;
     [SerializeField] private float scaleMax = .5f;
 
     public void Grab() {
         if (Grabbed) { return;  }
 
-        //search for the collider
         GameObject focusedObject = SphereCastedObject(Tags.GRABABLE, transform);
         if (focusedObject == null) { return; }
 
         if (focusedObject.GetComponent<spawner>()) focusedObject = focusedObject.GetComponent<spawner>().spawnObject();
 
+        if (focusedObject.transform.parent == otherHand.transform) {
+            scalingObject = focusedObject;
+            scaleMultip = scalingObject.transform.localScale.x;
+            startDistance = Vector3.Distance(transform.position, otherHand.transform.position);
+            return;
+        }
+
         grabbedObject = focusedObject;
         Grabbed = true;
         grabbedObject.transform.parent = transform;
 
-        if (grabbedObject.GetComponent<HairObject>() == null)
-        {
+        if (grabbedObject.GetComponent<HairObject>() == null) {
             grabbedObject.AddComponent<HairObject>();
         }
         grabbedObject.GetComponent<HairObject>().Lock(true);
     }
 
     public void Release() {
+        if (scalingObject != null) {
+            scalingObject = null;
+            return;
+        }
         if (!Grabbed) { return; }
         Grabbed = false;
-
+        
         Rigidbody grabbedRB = grabbedObject.GetComponent<Rigidbody>();
 
         //no other controller grabs it, so you can do things, else ignore.
-        if (grabbedObject.transform.parent == transform) 
-        {
+        if (grabbedObject.transform.parent == transform)  {
             grabbedObject.transform.parent = null;
             
-            if (grabbedObject.GetComponent<HairObject>().AttachedAtHead)
-            {
+            if (grabbedObject.GetComponent<HairObject>().AttachedAtHead) {
                 grabbedObject.transform.parent = grabbedObject.GetComponent<HairObject>().ParentTransform;
             } 
-            else
-            {
+            else {
                 //set gravity on
-                if (grabbedRB != null)
-                {
+                if (grabbedRB != null) {
                     grabbedObject.GetComponent<HairObject>().Lock(false);
 
                 }
@@ -60,16 +68,16 @@ public class Grabber : MonoBehaviour {
         }
     }
 
-    public void ScaleObject(float val) {
-        if (grabbedObject == null){ return; }
+    // public void ScaleObject(float val) {
+    //     if (grabbedObject == null){ return; }
 
-        Vector3 tempScale = grabbedObject.transform.localScale + Vector3.one * scaleSpeed * val;
+    //     Vector3 tempScale = grabbedObject.transform.localScale + Vector3.one * scaleSpeed * val;
 
-        if (tempScale.x < scaleMin) tempScale = new Vector3(scaleMin, scaleMin, scaleMin);
-        if (tempScale.x > scaleMax) tempScale = new Vector3(scaleMax, scaleMax, scaleMax);
+    //     if (tempScale.x < scaleMin) tempScale = new Vector3(scaleMin, scaleMin, scaleMin);
+    //     if (tempScale.x > scaleMax) tempScale = new Vector3(scaleMax, scaleMax, scaleMax);
 
-        grabbedObject.transform.localScale = tempScale;
-    }
+    //     grabbedObject.transform.localScale = tempScale;
+    // }
 
     public GameObject SphereCastedObject(string _tag, Transform _transform) {
 
@@ -97,17 +105,14 @@ public class Grabber : MonoBehaviour {
         return null;
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log("Grab");
-            Grab();
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log("Release");
-            Release();
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) Grab();
+        if (Input.GetKeyDown(KeyCode.R)) Release();
+
+        if(scalingObject != null) {
+            float scale = (Vector3.Distance(transform.position, otherHand.transform.position) / startDistance) * scaleMultip;
+            scale = Mathf.Clamp(scale, scaleMin, scaleMax);
+            scalingObject.transform.localScale = new Vector3(scale,scale,scale);
         }
     }
 
@@ -116,11 +121,5 @@ public class Grabber : MonoBehaviour {
         set {
             grabbed = value;
         }
-    }
-    void OnDrawGizmosSelected()
-    {
-        // Draw a yellow sphere at the transform's position
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, .2f);
     }
 }
