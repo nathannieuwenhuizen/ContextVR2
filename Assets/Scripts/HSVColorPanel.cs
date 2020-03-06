786f1f8b4248d5d589879a8f728ce5d4bffb20ab
Dutch <carnplaynl@gmail.com>
2020-03-06 16:57:04 +0100

    added swatches to colorpicker

--- Assets/Scripts/HSVColorPanel.cs
+++ Assets/Scripts/HSVColorPanel.cs
@@ -4,18 +4,58 @@ using UnityEngine.UI;
 public class HSVColorPanel : MonoBehaviour {
 
     public Color color;
+    [SerializeField] GameObject HSV;
+    [SerializeField] GameObject SWATCHES;
+
+    [Header("HSV SLIDERS")]
     [SerializeField] Slider Hue;
     [SerializeField] Slider Saturation;
     [SerializeField] Slider Value;
-
     [SerializeField] Image[] colorDisplay;
     [SerializeField] Texture2D SaturationBG;
     [SerializeField] Texture2D ValueBG;
+
+    [Header("SWATCHES")]
+    [SerializeField] int selectedColorIndex = 0;
+    [SerializeField] int swatchesWidth;
+    [SerializeField] float swatchesDistanceX;
+    [SerializeField] float swatchesDistanceY;
+    [SerializeField] Color[] swatches;
+    [SerializeField] GameObject swatchPrefab;
+    [SerializeField] Sprite normalSprite;
+    [SerializeField] Sprite selectedSprite;
+    GameObject[] swatchbuttons;
 
     private void Start() {
         ChaingedColor();
         updateSaturation();
         updateValue();
+        resetSwatches();
+    }
+
+    void resetSwatches() {
+        foreach (Transform child in SWATCHES.transform) GameObject.Destroy(child.gameObject);
+
+        swatchbuttons = new GameObject[swatches.Length];
+
+        for (int i = 0; i < swatches.Length; i++) {
+            swatchbuttons[i] = GameObject.Instantiate(swatchPrefab, SWATCHES.transform);
+            int xp = i;
+            int yp = i / swatchesWidth;
+            while (xp >= swatchesWidth) xp -= swatchesWidth;
+            swatchbuttons[i].GetComponent<RectTransform>().anchoredPosition += new Vector2(xp * swatchesDistanceX, -yp * swatchesDistanceY);
+            swatchbuttons[i].GetComponent<Image>().color = swatches[i];
+
+            int ii = i; //WHY DOES THIS WORK
+            swatchbuttons[i].GetComponent<Button>().onClick.AddListener(delegate { PressSwatch(ii); });
+        }
+    }
+
+    public void PressSwatch(int index) {
+        swatchbuttons[selectedColorIndex].GetComponent<Image>().sprite = normalSprite;
+        swatchbuttons[index].GetComponent<Image>().sprite = selectedSprite;
+        selectedColorIndex = index;
+        color = swatches[index];
     }
 
     public void ChaingedColor() {
@@ -37,5 +77,16 @@ public class HSVColorPanel : MonoBehaviour {
             ValueBG.SetPixel(i, 1, Color.HSVToRGB(Hue.value, Saturation.value, i / 32f));
         }
         ValueBG.Apply();
+    }
+
+    public void switchPanel() {
+        if (HSV.active) {
+            HSV.SetActive(false);
+            SWATCHES.SetActive(true);
+        }
+        else {
+            HSV.SetActive(true);
+            SWATCHES.SetActive(false);
+        }
     }
 }
