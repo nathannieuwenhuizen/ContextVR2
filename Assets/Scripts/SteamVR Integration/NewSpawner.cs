251f33bfeeac4533fc559b287a51319f293b09c1
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-03-13 15:08:55 +0100

    spawner new system test

---
+++ Assets/Scripts/SteamVR Integration/NewSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject SpawnableObject;
+    [SerializeField] HSVColorPanel colorPanel;
+    MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        renderer.material.color = colorPanel.color;
+    }
+
+    public void spawnObject()
+    {
+        GameObject obj = GameObject.Instantiate(this.gameObject, transform.position, transform.rotation);
+        Destroy(obj.GetComponent<spawner>());
+        obj.GetComponent<HairObject>().idleMaterial = GetComponent<HairObject>().idleMaterial;
+        //GameObject obj = GameObject.Instantiate(SpawnableObject, transform.position, transform.rotation);
+        obj.GetComponent<MeshRenderer>().material.color = colorPanel.color;
+    }
+}
