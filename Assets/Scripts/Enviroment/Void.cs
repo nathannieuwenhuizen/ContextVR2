807a887f4c8022683cba57cec9b789b41aeae90f
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-03-20 11:58:36 +0100

    adding barbershop enviroment

--- Assets/Scripts/Enviroment/Void.cs
+++ Assets/Scripts/Enviroment/Void.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public class Void : MonoBehaviour {
     private void OnCollisionEnter(Collision collision) {
         GameObject.Destroy(collision.gameObject);
-        Debug.Log("DESTROYED OBJECT");
     }
 }
