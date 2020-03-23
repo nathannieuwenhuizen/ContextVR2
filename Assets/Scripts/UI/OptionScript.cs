783716d1d8c4f174d57ab4ed7ad0c3bf135b1c41
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-03-23 23:46:36 +0100

    Added menu code for the settings that we currently have ingame (only volume) and play button

---
+++ Assets/Scripts/UI/OptionScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionScript : MonoBehaviour
+{
+    [SerializeField]
+    private Slider volumeSlider;
+
+    [SerializeField]
+    private Slider musicSlider;
+
+    private void Start()
+    {
+        AudioListener.volume = Settings.Volume;
+        volumeSlider.value = Settings.Volume;
+    }
+
+    public void UpdateVolume()
+    {
+        AudioManager.instance?.PlaySound(AudioEffect.scaleChange, 1);
+
+        Settings.Volume = volumeSlider.value;
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    public void UpdateMusic()
+    {
+        //Idk hoe dit geimplementeerd is
+    }
+
+    public void EnableSubs()
+    {
+        //We hebben geen subtitles
+    }
+
+}
