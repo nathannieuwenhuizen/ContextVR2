8134af1dc20880c35df13b7e58d40292607fa8d4
P4radigm <leon.vanoldenborgh@student.hku.nl>
2020-03-27 00:54:44 +0100

    Added customizable minscale and maxscale (add GrabbableScripts component to prop)

---
+++ Assets/Scripts/Ingame objects/GrabbableScripts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableScripts : MonoBehaviour
+{
+
+    [SerializeField] public float minScaleValue = 0.01f;
+    [SerializeField] public float maxScaleValue = 0.5f;
+
+    //[SerializeField] public float oultineThickness = 0.05f;
+}
